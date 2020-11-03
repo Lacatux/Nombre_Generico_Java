6188066a8ee5e0f2f455852971fe759492d27dea
Lacatux <jaimelacabexgonzalo@gmailc.om>
2020-11-03 11:50:55 +0100

    JFrame creado

--- src/Bingo.java
+++ src/Bingo.java
@@ -1,10 +1,40 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
-public class Bingo {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
+public class Bingo extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
-		
-		
-		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Bingo frame = new Bingo();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Bingo() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
 	}
 
 }
