bbdcfe2bd94409f7e81caeb55a6ec96912998ecc
Lacatux <jaimelacabexgonzalo@gmailc.om>
2020-11-10 14:18:27 +0100

    Carton not finished - 2 prim fil
    FUncionan tansolo las dos primeras filaaas

--- src/Bingo.java
+++ src/Bingo.java
@@ -138,155 +138,86 @@ public class Bingo extends JFrame {
 
 	private void crearBotones(JButton[][] arrayBotones) {
 
+		int numRnd;
+		int contFila;
 		for (int i = 0; i < FILAS; i++) {
 			for (int j = 0; j < COLUMNAS; j++) {
-				arrayBotones[i][j] = new JButton(introducir_numero(arrayBotones,j));
+				arrayBotones[i][j] = new JButton("");
 				arrayBotones[i][j].setFont(new Font("Tahoma", Font.BOLD, 69));
 			}
 		}
 
-		comprobarRepetidos(arrayBotones);
-
-		int col=0;
-		while(col<9) {
-			ordenarArray(arrayBotones,col);
-			col++;
+		for (int i = 0; i < 2; i++) {			
+			do {
+				contFila = 0;
+				for (int j = 0; j < COLUMNAS; j++) {
+					numRnd = random.nextInt(8) + 1;
+					if(numRnd < 5) {
+						arrayBotones[i][j].setEnabled(false);
+						contentPane.add(arrayBotones[i][j]); 
+						contFila++;
+					}else {
+						arrayBotones[i][j].setEnabled(true);
+						contentPane.add(arrayBotones[i][j]);
+					}					
+				}
+			} while(contFila != 4);
 		}
 
-		huecosVacios(arrayBotones);
 
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 9; j++) {
-				contentPane.add(arrayBotones[i][j]);
-			}
-		}
 
 	}
 
 	public String introducir_numero(JButton[][] array, int j) {
 		int numero=0;
 
-		switch(j){
-		case 0:{
+		switch (j) {
+		case 0:
 			numero=(int)(Math.random()*9+1);
 			break;
-		}
-		case 1:{
+
+		case 1:
 			numero=(int)(Math.random()*9+1);
 			numero=numero+10;
 			break;
-		}
-		case 2:{
+
+		case 2:
 			numero=(int)(Math.random()*9+1);
 			numero=numero+20;
 			break;
-		}
-		case 3:{
+
+		case 3:
 			numero=(int)(Math.random()*9+1);
 			numero=numero+30;
 			break;
-		}
-		case 4:{
+
+		case 4:
 			numero=(int)(Math.random()*9+1);
 			numero=numero+40;
 			break;
-		}
-		case 5:{
+
+		case 5:
 			numero=(int)(Math.random()*9+1);
 			numero=numero+50;
 			break;
-		}
-		case 6:{
+
+		case 6:
 			numero=(int)(Math.random()*9+1);
 			numero=numero+60;
 			break;
-		}
-		case 7:{
+
+		case 7:
 			numero=(int)(Math.random()*9+1);
 			numero=numero+70;
 			break;
-		}
-		case 8:{
+
+		case 8:
 			numero=(int)(Math.random()*9+1);
 			numero=numero+80;
 			break;
-		}
+
 		}			
 		return String.valueOf(numero);
-	}
-
-	public void comprobarRepetidos(JButton[][] array) {
-		int numero, cont=0;
-		boolean repetido=false;
-
-		for (int col = 0; col < 9; col++) {
-			do {
-				if(array[0][col].getText().equals(array[1][col].getText()) || array[0][col].getText().equals(array[2][col].getText())) {
-					numero=(int)(Math.random()*9+1);
-					numero=numero+cont;
-					array[0][col].setText(String.valueOf(numero));
-					repetido=true;
-				}else if(array[1][col].getText().equals(array[2][col].getText())) {
-					numero=(int)(Math.random()*9+1);
-					numero=numero+cont;
-					array[2][col].setText(String.valueOf(numero));
-					repetido=true;
-				}else {
-					repetido=false;
-				}
-			}while(repetido);
-			cont=cont+10;
-		}			
-	}
-
-	public void ordenarArray(JButton[][] array,int col) {
-		String[] enteros = new String[3];
-
-		for (int i = 0; i < 3; i++) {
-			enteros[i]=array[i][col].getText();
-		}
-
-		Arrays.sort(enteros);
-
-		for (int i = 0; i < 3; i++) {
-			array[i][col].setText(enteros[i]);
-		}
-
-	}
-
-	public void huecosVacios(JButton[][] array) {
-		int numero, huecoLinea=0, comprobarFila=0;
-
-		for (int fila = 0; fila < 3; fila++) {
-			huecoLinea=0;
-			if(fila==2) {
-				do {
-					numero=(int)(Math.random()*9+1);
-					if(!array[comprobarFila][numero-1].isEnabled() && !array[comprobarFila+1][numero-1].isEnabled()) {	
-					}else {
-						if(array[fila][numero-1].isEnabled()) {
-							array[fila][numero-1].setEnabled(false);
-							array[fila][numero-1].setBackground(Color.WHITE);
-							array[fila][numero-1].setText("");
-							huecoLinea++;
-						}
-					}
-
-				}while(huecoLinea!=4);
-				break;
-			}
-
-			do {
-				numero=(int)(Math.random()*9+1);
-				if(array[fila][numero-1].isEnabled()) {
-					array[fila][numero-1].setEnabled(false);
-					array[fila][numero-1].setBackground(Color.WHITE);
-					array[fila][numero-1].setText("");
-					huecoLinea++;
-				}			
-			}while(huecoLinea!=4);
-		}
-
 	}
 
 }
