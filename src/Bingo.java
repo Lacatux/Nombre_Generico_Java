7a96a281b81aefcdb35ca1d65aa6a113bf2b28f9
Lacatux <jaimelacabexgonzalo@gmailc.om>
2020-11-11 11:43:18 +0100

    CARTON CREADO
    MECAGOENMIPUTISIMAMADRE FINALMENTE TERMINO ESTA PUTISIMA BASURA

--- src/Bingo.java
+++ src/Bingo.java
@@ -138,86 +138,160 @@ public class Bingo extends JFrame {
 
 	private void crearBotones(JButton[][] arrayBotones) {
 
-		int numRnd;
-		int contFila;
+		int numRnd, contFila = 0, repetidos = 0;
 		for (int i = 0; i < FILAS; i++) {
 			for (int j = 0; j < COLUMNAS; j++) {
 				arrayBotones[i][j] = new JButton("");
 				arrayBotones[i][j].setFont(new Font("Tahoma", Font.BOLD, 69));
 			}
 		}
+		do {
+			for (int i = 0; i < 2; i++) {			
+				do {
+					contFila = 0;
+					repetidos = 0;
+					for (int j = 0; j < COLUMNAS; j++) {
+						numRnd = random.nextInt(8) + 1;
+						if(numRnd < 5 && contFila < 4) {
+							arrayBotones[i][j].setEnabled(false);
+							contentPane.add(arrayBotones[i][j]); 
+							contFila++;
+						}else {
+							arrayBotones[i][j].setEnabled(true);
+							contentPane.add(arrayBotones[i][j]);
+						}					
+					}
 
-		for (int i = 0; i < 2; i++) {			
-			do {
-				contFila = 0;
-				for (int j = 0; j < COLUMNAS; j++) {
-					numRnd = random.nextInt(8) + 1;
+
+				} while(contFila < 4);
+			}
+			for (int i = 0; i < COLUMNAS; i++) {
+				if(arrayBotones[0][i].isEnabled() && arrayBotones[1][i].isEnabled()) {
+					repetidos++;
+				}
+			}
+		} while (repetidos == 5);
+		do {
+			contFila = 0;
+			for (int i = 0; i < COLUMNAS; i++) {
+				numRnd = random.nextInt(8) + 1;
+				if ((arrayBotones[0][i].isEnabled() && arrayBotones[1][i].isEnabled()) && contFila < 4) {
+					arrayBotones[2][i].setEnabled(false);
+					contentPane.add(arrayBotones[2][i]); 
+					contFila++;
+				} else if ((arrayBotones[0][i].isEnabled() && !arrayBotones[1][i].isEnabled()) || (!arrayBotones[0][i].isEnabled() && arrayBotones[1][i].isEnabled())) {
 					if(numRnd < 5) {
-						arrayBotones[i][j].setEnabled(false);
-						contentPane.add(arrayBotones[i][j]); 
+						arrayBotones[2][i].setEnabled(false);
+						contentPane.add(arrayBotones[2][i]); 
 						contFila++;
 					}else {
-						arrayBotones[i][j].setEnabled(true);
-						contentPane.add(arrayBotones[i][j]);
-					}					
+						arrayBotones[2][i].setEnabled(true);
+						contentPane.add(arrayBotones[2][i]);
+					}	
+				} else if (!arrayBotones[0][i].isEnabled() && !arrayBotones[1][i].isEnabled()) {
+					arrayBotones[2][i].setEnabled(true);
+					contentPane.add(arrayBotones[2][i]);
+				} else {
+					arrayBotones[2][i].setEnabled(true);
+					contentPane.add(arrayBotones[2][i]);
 				}
-			} while(contFila != 4);
+
+				if (arrayBotones[0][i].isEnabled() && arrayBotones[1][i].isEnabled() && arrayBotones[2][i].isEnabled()) {
+					contFila = 5;
+				}
+			}
+		} while(contFila < 4 || contFila > 4);
+
+		for (int i = 0; i < FILAS; i++) {
+			for (int j = 0; j < COLUMNAS; j++) {
+				if (arrayBotones[i][j].isEnabled()) {
+					arrayBotones[i][j].setText(introducir_numero(arrayBotones, i, j));
+				}
+			}
+		}
+
+		ordenarArray(arrayBotones);
+
+	}
+
+	public String introducir_numero(JButton[][] array,int i, int j) {
+		int numero = 0;	
+		do {
+			numero = random.nextInt(9) + 1;
+			numero = numero + (10 * j);	
+		} while (comprobarArray(array, numero, i, j));
+		return String.valueOf(numero);
+	}
+
+	public boolean comprobarArray(JButton[][] array, int num, int i, int j) {
+
+		if (i == 0) {
+			return false;
+		} else if (i == 1) {
+			if(array[0][j].isEnabled()) {
+				int numArray = Integer.parseInt(array[0][j].getText());					
+				if (numArray == num) {
+					return true;
+				} 
+			} else {
+				return false;
+			}
+		} else if (i == 2) {
+			if(array[0][j].isEnabled()) {
+				int numArray = Integer.parseInt(array[0][j].getText());					
+				if (numArray == num) {
+					return true;
+				} 
+			} else if(array[1][j].isEnabled()) {
+				int numArray = Integer.parseInt(array[1][j].getText());					
+				if (numArray == num) {
+					return true;
+				} 
+			} else {
+				return false;
+			}
+		}
+
+		return false;
+	}
+
+	public void ordenarArray(JButton[][] array) {
+
+		int[] col;
+		int cont, contDos;
+		String num;
+		for (int j = 0; j < COLUMNAS; j++) {
+			cont = 0;
+			contDos = 0;
+
+			for (int i = 0; i < FILAS; i++) {
+				if (array[i][j].isEnabled()) 
+					cont++;
+			}
+
+			col = new int[cont];
+			for (int i = 0; i < FILAS; i++) {
+				if (array[i][j].isEnabled()) {
+					col[contDos] = Integer.parseInt(array[i][j].getText());
+					contDos++;
+				}
+			}
+
+			Arrays.sort(col);
+			contDos = 0;
+			for (int i = 0; i < FILAS; i++) {
+				if (array[i][j].isEnabled()) {
+					num = Integer.toString(col[contDos]);
+					array[i][j].setText(num);;
+					contDos++;
+				}
+			}
+
 		}
 
 
 
 	}
 
-	public String introducir_numero(JButton[][] array, int j) {
-		int numero=0;
-
-		switch (j) {
-		case 0:
-			numero=(int)(Math.random()*9+1);
-			break;
-
-		case 1:
-			numero=(int)(Math.random()*9+1);
-			numero=numero+10;
-			break;
-
-		case 2:
-			numero=(int)(Math.random()*9+1);
-			numero=numero+20;
-			break;
-
-		case 3:
-			numero=(int)(Math.random()*9+1);
-			numero=numero+30;
-			break;
-
-		case 4:
-			numero=(int)(Math.random()*9+1);
-			numero=numero+40;
-			break;
-
-		case 5:
-			numero=(int)(Math.random()*9+1);
-			numero=numero+50;
-			break;
-
-		case 6:
-			numero=(int)(Math.random()*9+1);
-			numero=numero+60;
-			break;
-
-		case 7:
-			numero=(int)(Math.random()*9+1);
-			numero=numero+70;
-			break;
-
-		case 8:
-			numero=(int)(Math.random()*9+1);
-			numero=numero+80;
-			break;
-
-		}			
-		return String.valueOf(numero);
-	}
 
 }
