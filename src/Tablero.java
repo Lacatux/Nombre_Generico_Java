c4042a206a918fd6e45def61a1fad8d456341dff
Lacatux <jaimelacabexgonzalo@gmailc.om>
2020-11-19 10:44:33 +0100

    FIN COOOOOOOOOOOOOOOOOOOOÑO

--- src/Tablero.java
+++ src/Tablero.java
@@ -7,20 +7,24 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Random;
 
 import javax.swing.JButton;
 
 public class Tablero extends JFrame {
 
-	private JLabel[][] arrayTablero;
+	private JLabel[] arrayTablero;
 	public static final int FILAS = 9;
 	public static final int COLUMNAS = 10;
 	private int[] numeros;
 	Random random = new Random();
-
+	private javax.swing.Timer timer;
+	private int cont = 0;
 	private JPanel contentPane;
 
 	/**
@@ -43,6 +47,24 @@ public class Tablero extends JFrame {
 	 * Create the frame.
 	 */
 	public Tablero() {
+		
+		
+		timer = new javax.swing.Timer(10000, new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				if (cont < 89) {
+					marcar(sacaBola(cont), cont);
+					cont++;
+				} else {
+					timer.stop();
+					JOptionPane.showMessageDialog(contentPane, "¡Ya se han recorrido todos los números!", "Aviso", EXIT_ON_CLOSE);
+				}
+
+			}
+		});
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("Bingo");
 		setResizable(false);
@@ -51,74 +73,63 @@ public class Tablero extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(new GridLayout(9, 10, 0, 0));
-
+		
 		crear();
+		
+		JLabel spacer = new JLabel("");
+		spacer.setForeground(Color.WHITE);
+		spacer.setBackground(Color.BLACK);
+		contentPane.add(spacer);
+		spacer.setOpaque(true);
 
-		numeros = new int[90];
-
-		comprobar();
-
-
+		
+		timer.start();
+		numeros = new int[89];
 	}
 
 	private void crear() {
 
-		arrayTablero = new JLabel[FILAS][COLUMNAS];
+		arrayTablero = new JLabel[89];
 		int cont = 1;
 		String contS;
-		for (int i = 0; i < FILAS; i++) {
-			for (int j = 0; j < COLUMNAS; j++) {
-				if(cont != 90) {
-					contS = Integer.toString(cont);
-					arrayTablero[i][j] = new JLabel(contS);
-					contentPane.add(arrayTablero[i][j]);
-					cont++;
-					arrayTablero[i][j].setFont(new Font("Tahoma", Font.BOLD, 25));
-					arrayTablero[i][j].setOpaque(true);
-				}
-			}
+		for (int i = 0; i < 89; i++) {
+
+			contS = Integer.toString(cont);
+			arrayTablero[i] = new JLabel(contS);
+			contentPane.add(arrayTablero[i]);
+			cont++;
+			arrayTablero[i].setFont(new Font("Tahoma", Font.BOLD, 40));
+			arrayTablero[i].setOpaque(true);
+			arrayTablero[i].setHorizontalAlignment(SwingConstants.CENTER);
+
 		}
 
 	}
 
-	private void bolas(int pos) {
+	private int sacaBola(int pos) {
 		boolean repetido = false;
-		if (pos < 90) {
-			do {
-				repetido = false;
-				if (pos == 0) {
-					numeros[pos] = random.nextInt(89) + 1;
-				} else {
-					numeros[pos] = random.nextInt(89) + 1;
-					for (int i = 0; i < pos; i++) {
-						if (numeros[pos] == numeros[i]) {
-							repetido = true;
-						}
-					}
-				}
-			} while (repetido);
-		}
-	}
 
-	private void comprobar() {
-		int num = 0;
-		int k = 0;
-
-		for (int i = 0; i < FILAS; i++) {
-			for (int j = 0; j < COLUMNAS; j++) {
-				
-					if(k < 89) {
-						bolas(k);
-						System.out.println(k);
-						num = Integer.parseInt(arrayTablero[i][j].getText());
-						if (numeros[k] == num) {
-							arrayTablero[i][j].setBackground(Color.BLACK);
-						}
-						k++;
+		do {
+			repetido = false;
+			if (pos == 0) {
+				numeros[pos] = random.nextInt(89) + 1;
+			} else {
+				numeros[pos] = random.nextInt(89) + 1;
+				for (int i = 0; i < pos; i++) {
+					if (numeros[pos] == numeros[i]) {
+						repetido = true;
+						break;
 					}
 				}
 			}
-		
+		} while (repetido);
+
+		return numeros[pos];
+	}
+
+	private void marcar(int numB, int cont) {
+		arrayTablero[numB - 1].setBackground(Color.BLACK);
+		arrayTablero[numB - 1].setForeground(Color.WHITE);
 	}
 
 }
